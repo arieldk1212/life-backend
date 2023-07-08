afe16cf21eb7b89cd20d7ba7052d19a4a2e58dbf
Ariel Kriheli <arieldk6@gmail.com>
2023-07-08 20:59:24 +0300

    added dev docker configuration

--- server/apps/events/admin.py
+++ server/apps/events/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import PersonalPhoto
 
-# Register your models here.
+
+admin.site.register(PersonalPhoto)
--- server/apps/events/models.py
+++ server/apps/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
+class PersonalPhoto(models.Model):
+  photo_belonging = models.ForeignKey(User, on_delete=models.CASCADE)
+  attachment = models.FileField(null=True, blank=True)
+  
